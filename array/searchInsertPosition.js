ab4191fd4d0979fa7d9c0cf83d4b831e678b6e48
Harsh Bajpai <hasrsh.bajpai1027@gmail.com>
2026-05-22 07:15:03 +0530

    FIXED: solved search insert position using Binary search

--- array/searchInsertPosition.js
+++ array/searchInsertPosition.js
@@ -11,3 +11,27 @@ var searchInsert = function(nums, target) {
    } 
    return nums.length;
 };
+
+// Optimized
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        }
+        else if (target < nums[mid]) {
+            end = mid - 1
+        }
+        else {
+            start = mid + 1
+        }
+    }
+    return start;
+};
